ea9288084dab68d5299f0d0de0fc3cf5bcf22441
ahmetsnyz <ahmetsnyz@gmail.com>
2021-12-21 20:45:01 +0300

    yeni proje eklendi

---
+++ LayoutKullanimi/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿//add>scrip>javascript file
+console.log("MVC Uygulaması Çalıştı");
+var App = {
+    name: "MVC Uygulaması",
+    selamla: function () {
+        return "Merhaba" + this.name;
+    },
+};
