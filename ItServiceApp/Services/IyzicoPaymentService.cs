c10e5f39e7f034256f16d3e66611485a3a9f4800
ahmetsnyz <ahmetsnyz@gmail.com>
2022-01-18 17:14:48 +0300

    payment service

--- ItServiceApp/Services/IyzicoPaymentService.cs
+++ ItServiceApp/Services/IyzicoPaymentService.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using ItServiceApp.Models.Payment;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,9 +30,35 @@ namespace ItServiceApp.Services
                 ThreedsCallbackUrl = section["ThreedsCallbackUrl"]
             };
         }
+        private string GenerateConversationId()
+        {
+            return MUsefulMethods.StringHelpers.GenerateUniqueCode();
+        }
         public InstalmentModel CheckInstallments(string binNumber, decimal price)
         {
-            throw new NotImplementedException();
+            if (binNumber.Length > 6)
+            {
+                binNumber = binNumber.Substring(0, 6);
+            }
+            var conversationId = GenerateConversationId();
+            var request = new RetrieveInstallmentInfoRequest
+            {
+                Locale = Locale.TR.ToString(),
+                ConversationId = conversationId,
+                BinNumber = binNumber,
+                Price = price.ToString(new CultureInfo("en-US"))
+            };
+            var result = InstallmentInfo.Retrieve(request, _options);
+            if (result.Status == "failure")
+            {
+                throw new Exception(result.ErrorMessage);
+            }
+            if (result.ConversationId != conversationId)
+            {
+                throw new Exception("Hatalı istek oluşturuldu");
+            }
+            var resultModel = _mapper.Map<InstalmentModel>(result.InstallmentDetails[0]);
+            return resultModel;
         }
 
         public PaymentResponseModel Pay(PaymentModel model)
