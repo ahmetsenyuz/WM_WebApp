5e8d073a9d80f935c4a88de746d23ee078cf6d2f
ahmetsnyz <ahmetsnyz@gmail.com>
2022-01-20 20:32:02 +0300

    admin paneli

---
+++ ItServiceApp/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")] //sadece rolü admin olan kullancının girişine izin veriliyr.
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ItServiceApp/Startup.cs
+++ ItServiceApp/Startup.cs
@@ -88,7 +88,11 @@ namespace ItServiceApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"); 
+                endpoints.MapAreaControllerRoute(
+                    name: "admin",
+                    areaName: "admin",
+                    pattern: "admin/{controller=Manage}/{action=Index}/{id?}");
             });
         }
     }
