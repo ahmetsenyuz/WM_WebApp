19f85d125ee84cfc24cacef3a89d75f34c0be554
ahmetsnyz <ahmetsnyz@gmail.com>
2022-02-01 18:55:53 +0300

    decimaller için ayar çekildi

--- ItServiceApp/Data/MyContext.cs
+++ ItServiceApp/Data/MyContext.cs
@@ -12,6 +12,16 @@ namespace ItServiceApp.Data
     public class MyContext : IdentityDbContext<ApplicationUser ,ApplicationRole, string>
     {
         public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)//decimal değerleri sqlde bozulmasın diye ayarlayacağız. öteki türlü virgülden sonra 18 hane alıyordu.
+        {
+            base.OnModelCreating(builder);//bu silinmeyecek kalacak!!!
+
+            //adı Fluent API
+            
+            builder.Entity<Subscription>().Property(x=> x.Amount).HasPrecision(9,2);//subscription tablosundali amount için düzenleme
+            builder.Entity<Subscription>().Property(x => x.PaidAmount).HasPrecision(9, 2);
+            builder.Entity<SubscriptionType>().Property(x=> x.Price).HasPrecision(9,2);
+        }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<State> States { get; set; }
